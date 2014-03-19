d9298a6beb085a08196769073ca3031a2d832950
Subin Siby <subins2000@gmail.com>
2014-03-19 13:13:37 +0530

    Version 0.1

---
+++ config.php
@@ -0,0 +1,12 @@
+<?
+$LC_config = array(
+ 'host' => 'localhost',
+ 'port' => '3306',
+ 'user' => 'root',
+ 'pass' => 'backstreetboys',
+ 'db' => 'lobbyLocal',
+ 'key' => 'o3tTKrBpp3NpTyCxvJcmbt4eKQaIZhLea4X',
+ 'prefix' => 'l_git'
+);
+$LC->debug(false);
+?>
