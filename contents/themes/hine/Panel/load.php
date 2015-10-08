99c9b01352928337b11beff3916578732629b565
Subin Siby <subins2000@gmail.com>
2015-10-08 17:22:24 +0530

    Updation to Theme

--- contents/themes/hine/Panel/load.php
+++ contents/themes/hine/Panel/load.php
@@ -2,12 +2,28 @@
 /**
  * Panel UI
  */
+$panelLeftItems = \Lobby\UI\Panel::getPanelItems("left");
 ?>
 <div class="panel top">
   <ul class="left">
-    <?php \Lobby\UI\Panel::panelItems("left"); ?>
+    <?php echo $this->makePanelTree("lobbyAdmin", $panelLeftItems["lobbyAdmin"]);unset($panelLeftItems["lobbyAdmin"]);?>
+    <?php 
+    $html = "";
+    foreach($panelLeftItems as $id => $item){
+      if( !isset($item['subItems']) ){
+        if( !isset($item['text']) && isset($item['html']) ){
+          $html .= $this->makePanelItem($item['html'], "htmlContent", $id, "prnt");
+        }else{
+          $html .= $this->makePanelItem($item['text'], $item['href'], $id, "prnt");
+        }
+     }else{
+        $html .= $this->makePanelTree($id, $item);
+      }
+    }
+    echo $html;
+    ?>
   </ul>
   <ul class="right">
-    <?php \Lobby\UI\Panel::panelItems("right"); ?>
+    <?php \Lobby\UI\Panel::getPanelItems("right"); ?>
   </ul>
 </div>
