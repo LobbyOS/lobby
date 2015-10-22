10217eeb6e770ca0fa430182e30cc28f6c2d7f67
Subin Siby <subins2000@gmail.com>
2015-10-22 12:42:14 +0530

    Some updates

--- contents/themes/hine/Panel/JS/panel.js
+++ contents/themes/hine/Panel/JS/panel.js
@@ -1,9 +1,6 @@
 $(document).ready(function(){
   $('.panel.top .left, .panel.top .right').superfish({
-    delay: 100,
-    onBeforeShow: function(){
-      $(this).css("margin-top", "10px");
-    }
+    delay: 100
   });
   $(document).tooltip({
     position: {
