642a1034bdf50479b62caeb4beaffa4fe6a2a1a2
subins2000 <subins2000@gmail.com>
2016-01-10 02:16:16 +0530

    sige updated

--- contents/apps/diary/App.php
+++ contents/apps/diary/App.php
@@ -5,7 +5,7 @@ class diary extends \Lobby\App {
   public function page($p){
     if(substr($p, 0, 7) == "/entry/"){
       $date = substr_replace($p, "", 0, 7);
-      return $this->inc("/src/Page/entry.php", array(
+      return $this->inc("/src/page/entry.php", array(
         "entry_date" => $date
       ));
     }else{
