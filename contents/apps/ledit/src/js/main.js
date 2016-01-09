a48aa976cf2902d6ac8e22aa484b7f164b6d84e4
subins2000 <subins2000@gmail.com>
2016-01-10 01:43:33 +0530

    lEdit app update

--- contents/apps/ledit/src/js/main.js
+++ contents/apps/ledit/src/js/main.js
@@ -9,29 +9,37 @@ function curDate() {
 }
 
 $(document).ready(function(){
+    tinymce.init({
+      selector: ".workspace#ledit #editor",
+      plugins: "autoresize"
+    });
    /* The App Parent Element URI */
    var app = ".workspace#ledit";
    
    $(app + " #save.button").live("click", function(){
-      if($(app + " #text").val() == ""){
-      $(app + " #error").text("Please Enter Something :-(").fadeIn().delay(2000).fadeOut();
+      if(tinyMCE.activeEditor.getContent() === ""){
+        $(app + " #error").text("Please Enter Something :-(").fadeIn().delay(2000).fadeOut();
       }else{
-      key = $(app + " #saveName").val(); // The key Name
-       value = $(app + " #text").val(); // The Content
-       if(key == ""){
-         key = curDate();
-       }
-       
-      lobby.app.save(key, value, function(data){
-          if(data == "bad"){
-            alert("Failed Saving Data");
-          }else{
-            lobby.app.ajax("saves.php", {}, function(data){
-               $(app + " #saves").html(data);
-            });
-            $(app + " #saved").fadeIn().delay(2000).fadeOut();
-          }
-      });
+        key = $(app + " #saveName").val(); // The key Name
+        value = tinyMCE.activeEditor.getContent(); // The Content
+        if(key == ""){
+          key = curDate();
+        }
+         
+        lobby.app.save(key, value, function(data){
+            if(data == "bad"){
+              alert("Failed Saving Data");
+            }else{
+              lobby.app.ajax("saves.php", {}, function(data){
+                 $(app + " #saves").html(data);
+              });
+              $(app).animate({backgroundColor: "rgb(101, 196, 53)"}, 1000, function(){
+                $(app).animate({backgroundColor: "rgb(255, 255, 255)"}, 1000, function(){
+                  $(app).attr("style", "");
+                });
+              });
+            }
+        });
       }
    });
    $(app+" #remove.button").live("click", function(){
