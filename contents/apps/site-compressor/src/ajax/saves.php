35e4db1d8cf98d5ad7761c3d03772dd332b5ca93
subins2000 <subins2000@gmail.com>
2016-01-13 17:51:23 +0530

    up

--- contents/apps/site-compressor/src/ajax/saves.php
+++ contents/apps/site-compressor/src/ajax/saves.php
@@ -4,7 +4,7 @@ $saves = getData("", "site-compressor");
 if( $saves ){
    foreach($saves as $save){
      $saveName = $save['name'];
-     $response[$saveName] = json_decode($save['content'], true);
+     $response[$saveName] = json_decode($save['value'], true);
    }
 }
 echo json_encode($response);
