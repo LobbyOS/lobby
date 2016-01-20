a84a7e99baa607605f8a16734e4cfe273d67afb4
subins2000 <subins2000@gmail.com>
2016-01-20 20:07:39 +0530

    Lobby 0.3

--- contents/apps/chatkin/src/ajax/connected.php
+++
@@ -1,7 +0,0 @@
-<?php
-if(isset($_POST['network']) && isset($_POST['id']) && isset($this->available_networks[$_POST['network']])){
-  H::saveJSONData("accounts", array(
-    $_POST['network'] => array($_POST['id'] => array(false))
-  ));
-  var_dump(H::getJSONData("accounts"));
-}
--- contents/apps/school-election/install.sh
+++
@@ -1,4 +0,0 @@
-echo "Bash version ${BASH_VERSION}..."
-sudo echo -n "" > /var/log/squid/access.log
-sudo usermod -G adm www-data
-sudo chown proxy:adm /var/log/squid/access.log
