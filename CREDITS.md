7bf756a365bee148c6b4ee900bc5463f1c3defd8
subins2000 <subins2000@gmail.com>
2016-01-31 21:53:26 +0530

    Apache license is strict about file names

--- CREDITS.md
+++
@@ -1,5 +0,0 @@
-# Developers Behind This Project
-
-* Subin Siby - http://subinsb.com
-* Suriyaa Kudo - http://j.mp/ItsSuriyaa
-* Anandu B Ajith - http://anandu.net/
--- contents/apps/site-compressor/src/ajax/saves.php
+++ contents/apps/site-compressor/src/ajax/saves.php
@@ -2,10 +2,10 @@
 $response = array();
 $saves = getData("", "site-compressor");
 if( $saves ){
-   foreach($saves as $save){
-     $saveName = $save['name'];
-     $response[$saveName] = json_decode($save['value'], true);
-   }
+  foreach($saves as $save){
+    $saveName = $save['name'];
+    $response[$saveName] = json_decode($save['value'], true);
+  }
 }
 echo json_encode($response);
 ?>
