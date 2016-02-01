6e9c86816c43bcf1c45dc0a13299430acb59c6b6
subins2000 <subins2000@gmail.com>
2016-02-01 19:03:40 +0530

    Tests

--- tests/LobbyInstall.php
+++ tests/LobbyInstall.php
@@ -1,5 +1,16 @@
 <?php
-class LobbyTest extends PHPUnit_Framework_TestCase{
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+
+class LobbyTest extends PHPUnit_Framework_TestCase {
+  
+  public function setUp(){
+    /**
+     * Start server
+     */
+    //$web = RemoteWebDriver::create("http://". WEB_SERVER_HOST . ":" . WEB_SERVER_PORT, DesiredCapabilities::firefox());
+    //$web->quit();
+  }
 
 	public function test() {
 		$this->assertEquals(1+1, 2);
