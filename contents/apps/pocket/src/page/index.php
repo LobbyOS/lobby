e193e3b5d04011041f197f472814c21e58ad0147
subins2000 <subins2000@gmail.com>
2016-02-02 08:23:35 +0530

    Pocket app

--- contents/apps/pocket/src/page/index.php
+++ contents/apps/pocket/src/page/index.php
@@ -1,1 +1,13 @@
-<div 
+<?php
+$this->addScript("app.js");
+$this->addStyle("app.css");
+?>
+<div class="contents">
+  <h1>Pocket</h1>
+  <div>
+    <div class="tpane">
+      <div id="balance"></div>
+      <div id="settings"></div>
+    </div>
+  </div>
+</div>
