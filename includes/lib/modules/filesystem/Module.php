0dc2ba71d6d6e4d0842d5abe281aa1d70430633b
subins2000 <subins2000@gmail.com>
2016-02-03 10:54:14 +0530

    File picker

--- includes/lib/modules/filesystem/Module.php
+++ includes/lib/modules/filesystem/Module.php
@@ -11,8 +11,8 @@ class FileSystem extends \Lobby\Module {
   public function init(){
     if(!\Lobby::status("lobby.serve")){
       \Lobby::hook("head.begin", function(){
-        \Lobby::addScript("Lobby.FS.filechooser", $this->dir . "/js/filechooser.js");
-        \Lobby::addStyle("Lobby.FS.filechooser", $this->dir . "/css/filechooser.css");
+        \Lobby::addScript("Lobby.FS.filepicker", $this->dir . "/filepicker/js/filepicker.js");
+        \Lobby::addStyle("Lobby.FS.filepicker", $this->dir . "/filepicker/css/filepicker.css");
       });
     }
   }
