df108348c54c9a8ab0697393a47d852f37afe66d
subins2000 <subins2000@gmail.com>
2016-02-24 21:33:10 +0530

    Changed Admin UI

--- contents/themes/hine/src/panel/load.admin.php
+++ contents/themes/hine/src/panel/load.admin.php
@@ -27,6 +27,24 @@ $panelLeftItems = \Lobby\UI\Panel::getPanelItems("left");
     ?>
   </ul>
   <ul class="right">
-    <?php \Lobby\UI\Panel::getPanelItems("right"); ?>
+    <?php
+    $panelRightItems = \Lobby\UI\Panel::getPanelItems("right");
+    $html = "";
+    foreach($panelRightItems as $id => $item){
+      if( !isset($item['subItems']) ){
+        if( !isset($item['text']) && isset($item['html']) ){
+          $html .= $this->makePanelItem($item['html'], "htmlContent", $id, "prnt");
+        }else{
+          $html .= $this->makePanelItem($item['text'], $item['href'], $id, "prnt");
+        }
+     }else{
+        $html .= $this->makePanelTree($id, $item);
+      }
+    }
+    echo $html;
+    ?>
   </ul>
+  <?php
+  \Lobby::doHook("panel.end");
+  ?>
 </nav>
