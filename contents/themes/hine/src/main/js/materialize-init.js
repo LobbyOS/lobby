1bc5f3c48459f07397d0a1be7070782ea5499341
subins2000 <subins2000@gmail.com>
2016-02-27 14:22:13 +0530

    Lobby 0.4.1

--- contents/themes/hine/src/main/js/materialize-init.js
+++ contents/themes/hine/src/main/js/materialize-init.js
@@ -5,20 +5,4 @@ lobby.load(function(){
     $(this).tooltip({delay: 50});
     $(this).attr("title", "");
   });
-  
-  /**
-   * Add <label> after 'select'
-   */
-  $('select').each(function(){
-    t=$(this);
-    if(!t.parents().hasClass("ui-datepicker")){
-      t.material_select();
-      t.live("change", function(){
-        t.material_select("destroy");
-        setTimeout(function(){
-          t.material_select();
-        }, 1000);
-      });
-    }
-  });
 });
