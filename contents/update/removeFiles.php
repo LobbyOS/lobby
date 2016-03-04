ddc186ff059a05f79a7fcb0f84c927eb5d89b012
subins2000 <subins2000@gmail.com>
2016-03-04 13:09:13 +0530

    Lobby 0.4.1

--- contents/update/removeFiles.php
+++ contents/update/removeFiles.php
@@ -1,3 +1,7 @@
+contents/themes/hine/src/css
+contents/themes/hine/src/lib
+includes/lib/jquery/ui
+includes/lib/jquery/jquery-ui.css
 includes/lib/lobby/css
 includes/lib/lobby/image
 includes/lib/lobby/ajax/app.php
