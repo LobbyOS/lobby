97bb117bc8e7375fdb6b5c0d3a23c88beb358acd
Subin Siby <subins2000@gmail.com>
2016-04-09 13:20:19 +0530

    Adding Notify

--- contents/themes/hine/src/panel/js/panel.js
+++ contents/themes/hine/src/panel/js/panel.js
@@ -9,4 +9,13 @@ lobby.load(function(){
   $("#notifyToggle").live("click", function(){
     $("#notifyBox").toggle("show");
   });
+  
+  lobby.notify.push = function(info){
+    pushItem = $("<div class='notifyItem row' id='notifyItem"+ info["id"] +"'><div class='col m2'><span></span></div><div class='col m10'>"+ info["contents"] +"</div></div>");
+    if(lobby.notify.box.find("#notifyItem" + info["id"]).length === 0){
+      pushItem.prependTo(lobby.notify.box);
+    }else{
+      lobby.notify.box.find("#notifyItem" + info["id"]).replaceWith(pushItem);
+    }
+  };
 });
