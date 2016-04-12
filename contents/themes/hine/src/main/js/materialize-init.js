e9aa6d53d26e22ca9776963c8a14690430bcf724
Subin Siby <subins2000@gmail.com>
2016-04-12 14:10:11 +0530

    Renamed

--- contents/themes/hine/src/main/js/materialize-init.js
+++
@@ -1,7 +0,0 @@
-lobby.load(function(){
-  $('[title]').each(function(){
-    $(this).attr("data-tooltip", $(this).attr("title"));
-    $(this).tooltip({delay: 50});
-    $(this).attr("title", "");
-  });
-});
