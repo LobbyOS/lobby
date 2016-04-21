59f6339c72da5daa8278693baaecf5d597defd1a
Subin Siby <subins2000@gmail.com>
2016-04-21 11:38:22 +0530

    Fixed bugs

--- includes/lib/lobby/js/main.js
+++ includes/lib/lobby/js/main.js
@@ -270,7 +270,8 @@ lobby.toast = function (message, displayLength, className, completeCallback) {
 lobby.notify = {
   box: null,
   
-  cehckInterval: null,
+  checkInterval: null,
+  nfs: [],
 
   init: function(){
     lobby.notify.box = $("nav #notifyBox");
@@ -294,8 +295,24 @@ lobby.notify = {
       }else{
         $.each(nfs, function(id, notification){
           notification["id"] = id;
+          lobby.notify.nfs.push(id);
           lobby.notify.push(notification);
         });
+        
+        /**
+         * Check if notify items have been removed
+         */
+        if(lobby.notify.nfs.length !== nfs.length){
+          $.each(lobby.notify.nfs, function(i, notifyID){
+            if(typeof nfs[notifyID] === "undefined"){
+              lobby.notify.push({
+                "id": notifyID,
+                "removed": true
+              });
+            }
+          });
+        }
+        
         lobby.notify.onNewItems();
       }
     });
