30563959927edbd9cb0969cbabf4dada78f4525c
Subin Siby <subins2000@gmail.com>
2016-05-21 12:36:37 +0530

    OOP for Requests/Response

--- includes/src/vendor/composer/autoload_psr4.php
+++ includes/src/vendor/composer/autoload_psr4.php
@@ -6,6 +6,8 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
+    'Symfony\\Component\\HttpFoundation\\' => array($vendorDir . '/symfony/http-foundation'),
     'Symfony\\Component\\Filesystem\\' => array($vendorDir . '/symfony/filesystem'),
     'Lobby\\UI\\' => array($vendorDir . '/lobby/lobby/src/Lobby/UI'),
     'Lobby\\' => array($vendorDir . '/lobby/lobby/src/Lobby'),
