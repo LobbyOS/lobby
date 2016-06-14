c7a08922c6f264babb7e65d6e9c08f107dedc2f8
Subin Siby <subins2000@gmail.com>
2016-06-14 20:45:21 +0530

    Added internal config

---
+++ includes/config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The online server of Lobby.
+ * Default : https://lobby.subinsb.com
+ */
+define("L_SERVER", "http://server.lobby.sim");
+
+/**
+ * The local directories
+ */
+define("APPS_DIR", L_DIR . "/contents/apps");
+define("THEMES_DIR", L_DIR . "/contents/themes");
