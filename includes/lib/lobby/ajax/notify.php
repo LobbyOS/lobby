9a33b151442cb96e258dff619ce85748e8b5c611
Subin Siby <subins2000@gmail.com>
2016-06-24 08:31:48 +0530

    Using new method to check if updates are available

--- includes/lib/lobby/ajax/notify.php
+++ includes/lib/lobby/ajax/notify.php
@@ -2,14 +2,9 @@
 $notifications = Lobby\DB::getJSONOption("notify_items");
 
 /**
- * If there is a update available either app or core, add an 
- * "Update Available" icon on the right side of panel
+ * If there is a update available either app or core, add a Notify item
  */
-$AppUpdates = json_decode(Lobby\DB::getOption("app_updates"), true);
-$lobby_version = \Lobby::$version;
-$latestVersion = Lobby\DB::getOption("lobby_latest_version");
-
-if((count($AppUpdates) != 0) || ($latestVersion && $lobby_version != $latestVersion)){
+if(\Lobby\Update::isAvailable()){
   $notifications["update"] = array(
     "contents" => "New Updates Are Available",
     "icon" => "update",
