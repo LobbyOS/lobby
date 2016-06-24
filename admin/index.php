a06f18d5447b794d1735450f7672e0f3572ffc5b
Subin Siby <subins2000@gmail.com>
2016-06-24 08:29:27 +0530

    Added FB group, Twitter link

--- admin/index.php
+++ admin/index.php
@@ -14,20 +14,26 @@
     <div id="workspace">
       <div class="contents">
         <h1>Admin</h1>
-        <p>Here, you can manage your Lobby installation :</p>
+        <?php
+        if(\Lobby\Update::isAvailable()){
+          echo sss("Updates Available", "Some updates are available for you to update. Yay!<cl/><a class='btn blue' href='". \Lobby::u("/admin/update.php") ."'>See Updates</a>");
+        }
+        ?>
+        <p>Manage your Lobby installation.</p>
         <?php
         echo \Lobby::l("admin/settings.php", "Settings", "class='btn red'") . "&nbsp;";
-        echo \Lobby::l("admin/apps.php", "Installed Apps", "class='btn green'") . "&nbsp;";
-        echo \Lobby::l("admin/lobby-store.php", "Lobby Store", "class='btn blue'") . "&nbsp;";
+        echo \Lobby::l("admin/apps.php", "Apps", "class='btn green'") . "&nbsp;";
+        echo \Lobby::l("admin/lobby-store.php", "Lobby Store", "class='btn pink'") . "&nbsp;";
         if(\Lobby\Modules::exists("admin")){
           echo \Lobby::l("admin/login?logout=true", "Log Out", "class='btn'");
         }
         ?>
-        <h2>Thank You</h2>
-        <p>We, the <a href="https://github.com/orgs/LobbyOS/people" target="_blank">Lobby Team</a> would like to thank you for installing Lobby.</p>
-        <p>Lobby is in <b>public beta mode</b>, so bugs may exist and some things may not work for you.</p>
-        <p>As you know, Lobby is an <a href="https://en.wikipedia.org/wiki/Open-source_software" target="_blank">Open Source Software</a>. We will be very glad and happy if you report <b>any kinds of problems/bugs</b> you faced :</p>
-        <p>Encoutered a problem or want to make a suggestion ? Please do it on our <a target="_blank" class="btn orange" href="https://github.com/subins2000/lobby/issues">GitHub Repo</a></p>
+        <h2>About</h2>
+        <p>You are using <b>Lobby <?php echo \Lobby::getVersion();?></b>.</p>
+        <p>Lobby is an Open Source software. <b>Feel free</b> to ask your doubts and problems on our Facebook Group or GitHub</p>
+        <a target="_blank" class="btn pink" href="https://www.facebook.com/groups/LobbyOS">Facebook</a>
+        <a target="_blank" class="btn blue" href="https://twitter.com/LobbyOS">Twitter</a>
+        <a target="_blank" class="btn black" href="https://github.com/subins2000/lobby/issues">GitHub</a>
       </div>
     </div>
   </body>
