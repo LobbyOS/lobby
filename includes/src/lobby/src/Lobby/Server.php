2fd5b7a05b3c48d1f4652aed493f200731b51103
Subin Siby <subins2000@gmail.com>
2016-06-25 11:36:32 +0530

    Server can now add notify items

--- includes/src/lobby/src/Lobby/Server.php
+++ includes/src/lobby/src/Lobby/Server.php
@@ -6,6 +6,7 @@
 namespace Lobby;
 
 use Lobby\Apps;
+use Lobby\UI\Panel;
 
 class Server {
 
@@ -81,8 +82,8 @@ class Server {
       \Lobby::log("Checkup with server failed ($url) : $error");
       $response = false;
     }
+    
     if($response){
-      
       $response = json_decode($response, true);
       if(is_array($response)){
         DB::saveOption("lobby_latest_version", $response['version']);
@@ -98,6 +99,19 @@ class Server {
             }
           }
           DB::saveOption("app_updates", json_encode($AppUpdates));
+        }
+        
+        if(isset($response["notify"])){
+          foreach($response["notify"]["items"] as $itemID => $item){
+            if(isset($item["href"])){
+              $item["href"] = \Lobby::u($item["href"]);
+            }
+            Panel::addNotifyItem("lobby_server_msg_" . $itemID, $item);
+          }
+          
+          foreach($response["notify"]["remove_items"] as $itemID){
+            Panel::removeNotifyItem("lobby_server_msg_" . $itemID);
+          }
         }
       }
     }
