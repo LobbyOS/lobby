a4ea35e4eb7801d42c2e9f28a6df7836cc9acdd8
Subin Siby <subins2000@gmail.com>
2016-07-17 14:20:52 +0530

    Added tests

--- testing/autoload.php
+++ testing/autoload.php
@@ -3,15 +3,18 @@ require_once __DIR__ . "/vendor/autoload.php";
 
 use Neutron\TemporaryFilesystem\TemporaryFilesystem;
 
-$GLOBALS["FS"] = TemporaryFilesystem::create();
-
-define("WEB_SERVER_DOCROOT", $GLOBALS["FS"]->createTemporaryDirectory($mode = 0755));
-
-exec("cp -R '". realpath(__DIR__ . "/../") . "/.' '". WEB_SERVER_DOCROOT ."'");
+if(file_exists(sys_get_temp_dir() . "/lobby-tmp-loc.txt")){
+  define("WEB_SERVER_DOCROOT", file_get_contents(sys_get_temp_dir() . "/lobby-tmp-loc.txt"));
+}else{
+  $GLOBALS["FS"] = TemporaryFilesystem::create();
+  define("WEB_SERVER_DOCROOT", $GLOBALS["FS"]->createTemporaryDirectory($mode = 0755));
+  
+  exec("cp -R '". realpath(__DIR__ . "/../") . "/.' '". WEB_SERVER_DOCROOT ."'");
+  unlink(WEB_SERVER_DOCROOT . "/config.php");
+  file_put_contents(sys_get_temp_dir() . "/lobby-tmp-loc.txt", WEB_SERVER_DOCROOT);
+}
 
 echo "Mocking filesytem at " . WEB_SERVER_DOCROOT . PHP_EOL;
-
-unlink(WEB_SERVER_DOCROOT . "/config.php");
 
 /**
  * Start Server
---
+++ testing/tests/InstallApp.php
@@ -0,0 +1,47 @@
+<?php
+use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+
+class InstallApp extends PHPUnit_Framework_TestCase {
+  
+  public function setUp(){
+    /**
+     * Configure Selenium server
+     */
+    $this->driver = RemoteWebDriver::create("http://localhost:4444/wd/hub", DesiredCapabilities::firefox());
+    
+    exec("find '". WEB_SERVER_DOCROOT ."/contents/apps' -mindepth 1 -exec rm -rf '{}' \+");
+  }
+  
+  public function testCSSLoad(){
+    $this->driver->get("http://". WEB_SERVER_HOST .":". WEB_SERVER_PORT . "/admin/lobby-store.php");
+    $src = $this->driver->getPageSource();
+    $cssURL = $this->driver->findElement(WebDriverBy::cssSelector("link"))->getAttribute("href");
+    
+    $this->driver->get($cssURL);
+    $this->assertContains("#storeNav", $this->driver->getPageSource());
+  }
+
+	public function testCanBeInstalled(){
+    /**
+     * Install intro
+     */
+    $this->driver->get("http://". WEB_SERVER_HOST .":". WEB_SERVER_PORT . "/admin/lobby-store.php?app=anagram");
+    $this->assertNotEquals($this->driver->findElement(WebDriverBy::cssSelector("#leftpane .btn.red"))->getAttribute("class"), "btn red disabled");
+  }
+  
+  public function testInstallByAJAX(){
+    $this->driver->get("http://". WEB_SERVER_HOST .":". WEB_SERVER_PORT . "/admin/lobby-store.php?app=anagram");
+    $this->driver->findElement(WebDriverBy::cssSelector("#leftpane .btn.red"))->click();
+    
+    new WebDriverWait($this->driver, 5);
+    
+    $this->assertContains($this->driver->getPageSource();
+  }
+  
+  public function tearDown(){
+    $this->driver->close();
+  }
+  
+}
