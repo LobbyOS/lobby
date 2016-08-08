cb2082f705a616d144fdf086373c37c5c1a6d062
Subin Siby <subins2000@gmail.com>
2016-08-08 21:02:19 +0530

    Made tests better

---
+++ testing/setup-tests.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__ . "/vendor/autoload.php";
+
+use Neutron\TemporaryFilesystem\TemporaryFilesystem;
+
+$FS = TemporaryFilesystem::create();
+define("WEB_SERVER_DOCROOT", $FS->createTemporaryDirectory($mode = 0755));
+
+echo "Making the lobby web server docroot";
+
+exec("cp -R '". realpath(__DIR__ . "/../") . "/.' '". WEB_SERVER_DOCROOT ."'");
+unlink(WEB_SERVER_DOCROOT . "/config.php");
+file_put_contents(sys_get_temp_dir() . "/lobby-tmp-loc.txt", WEB_SERVER_DOCROOT);
+?>
