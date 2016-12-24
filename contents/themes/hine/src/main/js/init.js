92863da94ae59769e6e365f324aa9530325f046f
Subin Siby <subins2000@gmail.com>
2016-12-25 02:27:58 +0530

    Removed tooltip
    Browser performance was hit because of tooltip and
    it's animations. Besides it was annoying

--- contents/themes/hine/src/main/js/init.js
+++ contents/themes/hine/src/main/js/init.js
@@ -1,10 +1,4 @@
 lobby.load(function(){
-  $('[title]').tooltip({
-    content: function(){
-      return $(this).attr('title');
-    }
-  });
-
   $("nav .button-collapse").sideNav({
     menuWidth: 200
   });
